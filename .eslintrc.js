cb83701ff18f0505e41cd5d592e840b345eb0db8
Lenny Peters <lpeters3@lululemon.com>
2020-04-10 14:46:06 -0700

    fix: 🐛 .eslintrc
    returned back for editing

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {},
+};
